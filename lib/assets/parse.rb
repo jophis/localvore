2ae631e79769e8f318510eeb1672060613bb7e42
Rickytony <karp.richard@gmail.com>
2014-05-14 17:14:19 -0400

    scrape and task

--- lib/assets/parse.rb
+++ lib/assets/parse.rb
@@ -22,19 +22,25 @@ product_table.each do |table|
   categories[key] =  product_array
 end
 
-p links
 
-
+list_page = Nokogiri::HTML(open('http://greenbeltfresh.ca/grower-producer-list'))
+links = list_page.css(".even a").map { |link| link['href'] }
 links.each do |url|
     page = Nokogiri::HTML(open("http://greenbeltfresh.ca/#{url}.html"))
     name = page.css(".div_profile h1")
-    location = page.at_css("div .right_profile_text").children
     address = [location[0], location[2]].join(", ")
     phone_number = location[6][1]
     description = page.at_css(".div_profile p:nth-of-type(3)")
     website = page.at_css(".web a")
-    product_table = page.css(".products_delivery_table_grower")
     categories.each { |category, product| }
 
-    # create da model here
+    Farm.create!(
+      name: name,
+      address: address, 
+      phone_number: phone_number,
+      description: description,
+      website: website,
+
+
+      )
   end
